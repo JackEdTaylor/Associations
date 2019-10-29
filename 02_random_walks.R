3334c6aaf894dae18c47b154ed7c3d5f716656e4
Jack Taylor <jackt@psy.gla.ac.uk>
2019-10-29 11:38:40 +0000

    support random walks

--- 02_random_walks.R
+++ 02_random_walks.R
@@ -3,4 +3,57 @@ library(tidyverse)
 # import data
 swow <- read_csv("00_swow_edited.csv")
 
+build_network <- function(cue_node, n_walks = 5, alpha = .95, .swow = swow, assoc_weight = "ppmi", cues_only = TRUE) {
+  
+  # NOTE: assoc_weight should be the column that reflects association strength (i.e. "ppmi" or "R123.Strength")
+  
+  library(purrr)
+  
+  # if cues_only is TRUE, responses are removed if they are never used as a cue
+  # setting cues_only to FALSE will cause walks to terminate at non-cue responses, such that all subsequent steps in that walk will return NA
+  if (cues_only) .swow <- filter(.swow, response %in% cue)
+  
+  # alpha = probability at any step on a walk taking another step
+  cat(sprintf("Calculating walk lengths for alpha of %s...", alpha))
+  walk_lengths <- lapply(1:n_walks, function(walk_nr) {
+    decayed <- FALSE
+    walk_length <- 0
+    while(!decayed) {
+      decayed <- base::sample(c(TRUE, FALSE), 1, prob = c(1 - alpha, alpha))
+      walk_length <- walk_length + 1
+    }
+    walk_length
+  })
+  cat(sprintf("  -done\n"))
+  
+  # function to take one step in a random walk
+  take_step <- function(node_i, step_nr) {
+    if (is.na(node_i)) {
+      NA
+    } else {
+      # get all responses to this cue
+      cue_dat <- filter(.swow, cue == node_i)
+      # get the next step
+      if (nrow(cue_dat) > 0) {
+        cue_dat %>%
+          dplyr::sample_n(1, weight = !!dplyr::sym(assoc_weight)) %>%
+          dplyr::pull(response)
+      } else {
+        NA
+      }
+    }
+  }
+  
+  cat(sprintf("Taking %i walks...", n_walks))
+  out <- purrr::map(walk_lengths, ~ purrr::accumulate(1:., take_step, .init = cue_node))
+  cat(sprintf("  -done\n"))
+  
+  out
+  
+}
 
+library(tictoc)
+
+tic("100 steps")
+build_network("bridge", n_walks = 100)
+toc()
